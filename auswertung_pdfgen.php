8ca11dd65f405597c3f5149211a0ba9f280b2761
Lars Bleckwenn <lars@bleckwenn.net>
2015-10-08 14:55:46 +0200

    Summe in PDF Auswertung
    In der PDF Auswertung wurde eine Summenzeile ergänzt. Der Zeilenabstand
    musste dafür verringert werden. Alle Monate sollen 31 Tage haben.
    Die zusätzliche Seite mit den ggf. weiteren Informationen zu den
    Kopfschmerztagen sollt nicht mehr ausgegeben werden.

--- auswertung_pdfgen.php
+++ auswertung_pdfgen.php
@@ -66,72 +66,85 @@ for($m = $n; $m < $n + 6; $m ++) {
 	$pdf->Cell ( 3, 3, 3, B, 0, C, 1 );
 	$pdf->Cell ( 3, 3, 4, BR, 0, C, 1 );
 }
+$height = 4.85;
 $pdf->Ln ();
 $pdf->SetFillColor ( 229, 233, 240 );
 for($t = 1; $t < 32; $t ++) {
 	for($m = $n; $m < $n + 6; $m ++) {
-		if ($t > date ( 't', mktime ( 0, 0, 0, $m, 1, $Y ) )) {
-			$pdf->Cell ( 46, 5, '', ($m == $n) ? 0 : L );
-		} else {
-			$pdf->Cell ( 6, 5, $t, 1, 0, R, $m % 2 );
-			$ks_day = mktime ( 0, 0, 0, $m, $t, $Y );
-			if (isset ( $tage [$ks_day] )) {
-				$ks_data = $tage [$ks_day];
-				$pdf->Cell ( 3, 5, in_array ( 'm', $ks_data ['ks_art'] ) ? 'X' : '', 1, 0, C, $m % 2 );
-				$pdf->Cell ( 3, 5, in_array ( 's', $ks_data ['ks_art'] ) ? 'X' : '', 1, 0, C, $m % 2 );
-				$pdf->Cell ( 3, 5, in_array ( 'u', $ks_data ['ks_art'] ) ? 'X' : '', 1, 0, C, $m % 2 );
-				$mediliste = '';
-				foreach ( $ks_data ['ks_medis'] as $medi ) {
-					$medi_name_kurz = kurzname ( $medikamente, $medi );
-					$mediliste .= $medi_name_kurz . ', ';
-					$legende [$medi_name_kurz] = $medikamente [$medi];
-				}
-				$pdf->Cell ( 16, 5, substr ( $mediliste, 0, - 2 ), 1, 0, L, $m % 2 );
-				for($g = 0; $g < 5; $g ++)
-					$pdf->Cell ( 3, 5, ($ks_data ['ks_grad'] == $g) ? 'X' : '', 1, 0, C, $m % 2 );
-			} else {
-				$pdf->Cell ( 3, 5, '', 1, 0, L, $m % 2 );
-				$pdf->Cell ( 3, 5, '', 1, 0, L, $m % 2 );
-				$pdf->Cell ( 3, 5, '', 1, 0, L, $m % 2 );
-				$pdf->Cell ( 16, 5, '', 1, 0, L, $m % 2 );
-				for($g = 0; $g < 5; $g ++)
-					$pdf->Cell ( 3, 5, '', 1, 0, L, $m % 2 );
+		// 'IF' auskommentiert, da alle Monate 31 Tage haben sollen
+		// if ($t > date ( 't', mktime ( 0, 0, 0, $m, 1, $Y ) )) {
+		// $pdf->Cell ( 46, $height, '', ($m == $n) ? 0 : L );
+		// } else {
+		$pdf->Cell ( 6, $height, $t, 1, 0, R, $m % 2 );
+		$ks_day = mktime ( 0, 0, 0, $m, $t, $Y );
+		if (isset ( $tage [$ks_day] )) {
+			$ks_data = $tage [$ks_day];
+			if (sizeof ( $ks_data ['ks_art'] ) > 0)
+				$sek [$m] ++;
+			$pdf->Cell ( 3, $height, in_array ( 'm', $ks_data ['ks_art'] ) ? 'X' : '', 1, 0, C, $m % 2 );
+			$pdf->Cell ( 3, $height, in_array ( 's', $ks_data ['ks_art'] ) ? 'X' : '', 1, 0, C, $m % 2 );
+			$pdf->Cell ( 3, $height, in_array ( 'u', $ks_data ['ks_art'] ) ? 'X' : '', 1, 0, C, $m % 2 );
+			$mediliste = '';
+			if (sizeof ( $ks_data ['ks_medis'] ) > 0)
+				$sem [$m] ++;
+			foreach ( $ks_data ['ks_medis'] as $medi ) {
+				$medi_name_kurz = kurzname ( $medikamente, $medi );
+				$mediliste .= $medi_name_kurz . ', ';
+				$legende [$medi_name_kurz] = $medikamente [$medi];
 			}
+			$pdf->Cell ( 16, $height, substr ( $mediliste, 0, - 2 ), 1, 0, L, $m % 2 );
+			for($g = 0; $g < 5; $g ++)
+				$pdf->Cell ( 3, $height, ($ks_data ['ks_grad'] == $g) ? 'X' : '', 1, 0, C, $m % 2 );
+		} else {
+			$pdf->Cell ( 3, $height, '', 1, 0, L, $m % 2 );
+			$pdf->Cell ( 3, $height, '', 1, 0, L, $m % 2 );
+			$pdf->Cell ( 3, $height, '', 1, 0, L, $m % 2 );
+			$pdf->Cell ( 16, $height, '', 1, 0, L, $m % 2 );
+			for($g = 0; $g < 5; $g ++)
+				$pdf->Cell ( 3, $height, '', 1, 0, L, $m % 2 );
+			// }
 		}
 	}
 	$pdf->Ln ();
 }
+for($m = $n; $m < $n + 6; $m ++) {
+	$pdf->Cell ( 6, 5, 'Se.', 1, 0, C, $m % 2 );
+	$pdf->Cell ( 9, 5, $sek [$m], 1, 0, C, $m % 2 );
+	$pdf->Cell ( 16, 5, $sem[$m], 1, 0, C, $m % 2 );
+	$pdf->Cell ( 15, 5, '', 1, 0, C, $m % 2 );
+}
+$pdf->Ln ();
 if (is_array ( $legende )) {
 	foreach ( $legende as $kurz => $lang ) {
 		$legende_text .= "$kurz = $lang, ";
 	}
 }
 $pdf->Cell ( 0, 8, substr ( $legende_text, 0, - 2 ), 0, 0, C );
-$pdf->Ln ();
 
-$pdf->SetLeftMargin ( 15 );
-$pdf->SetTopMargin ( 15 );
-$pdf->SetFont ( 'Arial', 'B', 14 );
-$pdf->AddPage ( 'P' );
-$pdf->Cell ( 0, 10, 'Weitere Eintragungen zu den umseitigen Kopfschmerztagen' );
-$pdf->Ln ();
-$pdf->SetFont ( 'Arial', 'B', 12 );
-$pdf->Cell ( 30, 6, 'Datum', RB );
-$pdf->Cell ( 0, 6, 'Information / Bemerkung', RB );
-$pdf->Ln ();
-$pdf->SetFont ( 'Arial', '', 12 );
-foreach ( $tage as $tag => $data ) {
-	if ($data ['ks_info'] == "")
-		continue;
-	$pdf->Cell ( 30, 6, date ( 'd.m.Y', $tag ), RB );
-	$pdf->Cell ( 0, 6, utf8_decode(html_entity_decode($data ['ks_info'], ENT_QUOTES)), RB );
-	$pdf->Ln ();
-}
-// $smarty->assign ( "monate", $monate );
-// $smarty->assign ( "tage", $tage );
+// Programmcode für die zu den Kopfschmerztagen gespeicherten Informationen
+// $pdf->Ln ();
+// $pdf->SetLeftMargin ( 15 );
+// $pdf->SetTopMargin ( 15 );
+// $pdf->SetFont ( 'Arial', 'B', 14 );
+// $pdf->AddPage ( 'P' );
+// $pdf->Cell ( 0, 10, 'Weitere Eintragungen zu den umseitigen Kopfschmerztagen' );
+// $pdf->Ln ();
+// $pdf->SetFont ( 'Arial', 'B', 12 );
+// $pdf->Cell ( 30, 6, 'Datum', RB );
+// $pdf->Cell ( 0, 6, 'Information / Bemerkung', RB );
+// $pdf->Ln ();
+// $pdf->SetFont ( 'Arial', '', 12 );
+// foreach ( $tage as $tag => $data ) {
+// if ($data ['ks_info'] == "")
+// continue;
+// $pdf->Cell ( 30, 6, date ( 'd.m.Y', $tag ), RB );
+// $pdf->Cell ( 0, 6, utf8_decode(html_entity_decode($data ['ks_info'], ENT_QUOTES)), RB );
+// $pdf->Ln ();
+// }
+
 $sm = strftime ( '%b%y', mktime ( 0, 0, 0, $n, 1, $Y ) );
 $em = strftime ( '%b%y', mktime ( 0, 0, 0, $n + 5, 1, $Y ) );
-$pdf->Output ( "KSK_$sm-$em", I );
+$pdf->Output ( "KSK_$sm-$em.pdf", I );
 $seite = "auswertung";
 include ('auswertung.php');
 function kurzname($medikamente, $med_id) {
